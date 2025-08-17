6ded5088c2ed83923a3174f99726fa010ca274a0
Sozhavaram Srivatasan <sozhavaramsrivatsan@hotmail.com>
2025-08-17 21:29:24 +0530

    added user view, urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username, password)
+        if user:
+            return  Response({"message": "Login Successful"}, status= status.HTTP_200_OK)
+        return  Response({"message": "Login Failed"}, status= status.HTTP_401_UNAUTHORIZED)
+class ProfileView(APIView):
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
