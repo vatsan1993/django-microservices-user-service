55e04f8347d376f158d193c0769e7fee3cbda6be
Sozhavaram Srivatasan <sozhavaramsrivatsan@hotmail.com>
2025-08-17 20:51:41 +0530

    added custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=10, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
